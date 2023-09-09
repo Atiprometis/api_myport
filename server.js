99e1705c48f7ae0c27da692c2dcd7f3cb05e5dd9
Fang <fang.live-@msn.com>
2023-09-09 14:23:08 +0700

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var cors = require('cors')
 
 const mysql2 = require('mysql2')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({
   extended: true
