86ad73b8c4883d87d7be13e58114d0c52621a35c
Fang <fang.live-@msn.com>
2024-08-19 11:28:11 +0700

    add read update

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ connection.connect((err)=>{
   }
   console.log('mysql connection');
 })
+// === create table ===
+
 
 // post data
 app.post("/create", async (req, res)=>{
@@ -68,6 +70,68 @@ app.get("/read", async (req, res) => {
   }
 })
 
+// red about me
+app.get("/readaboutme", async(req, res)=>{
+
+  try{
+    connection.query(
+      "SELECT * FROM aboutme WHERE id = 2",(err, results, fields)=>{
+      if(err){
+        console.log(err);
+        return res.status(400).send();
+      }
+      res.status(200).json(results);
+    }
+    )
+      }catch(err){
+        console.log(err);
+        return res.status(500).send();
+      }
+  
+})
+
+// read exp
+
+app.get("/readexp", async(req, res)=>{
+
+  try{
+    connection.query(
+      "SELECT * FROM exp ORDER BY id_exp DESC",(err, results, fields)=>{
+      if(err){
+        console.log(err);
+        return res.status(400).send();
+      }
+      res.status(200).json(results);
+    }
+    )
+      }catch(err){
+        console.log(err);
+        return res.status(500).send();
+      }
+  
+})
+
+// read education
+
+app.get("/readeducation", async(req, res)=>{
+
+  try{
+    connection.query(
+      "SELECT * FROM education ORDER BY id_edu DESC",(err, results, fields)=>{
+      if(err){
+        console.log(err);
+        return res.status(400).send();
+      }
+      res.status(200).json(results);
+    }
+    )
+      }catch(err){
+        console.log(err);
+        return res.status(500).send();
+      }
+  
+})
+
 // read single result
 app.get("/read/single/:id", async (req, res) => {
   const id = req.params.id;
@@ -89,19 +153,32 @@ app.get("/read/single/:id", async (req, res) => {
 })
 
 // update 
-app.patch("/update/:id", async (req, res ) => {
 
-  const id = req.params.id;
-  const newData = req.body.newData;
+
+app.patch("/update/aboutme", async (req, res ) => {
+
+console.log(req.body);
+console.log(req.body.id);
+console.log(req.body.content);
+
+  const id = req.body.id;
+  const content = req.body.content;
+
+  
+  // res.send(`test update =  ${content} + id = ${id} `);
 
   try{
     connection.query(
-      "UPDATE users SET data = ? WHERE id = ?",[newData,id], (err, results, fields) => {
+      `UPDATE aboutme SET id = ?, content = ?`,[id,content], (err, results, fields) => {
         if(err){
           console.log(err);
           return res.status(400).send();
         } 
         res.status(200).json({message: "user new data successfully"});
+      },{
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
       }
     )
 
@@ -111,6 +188,9 @@ app.patch("/update/:id", async (req, res ) => {
   }
 
 })
+
+//update aboutme
+
 
 // delete 
 app.delete("/delete/:id", async (req, res) =>{
@@ -137,6 +217,30 @@ app.delete("/delete/:id", async (req, res) =>{
 
 })
 
+// เทสๆ 
+
+app.get("/test",(req, res)=>{
+  console.log(req.query);
+  const {aa,bb} = req.query;
+  res.send(`madi aa=${aa},${bb}`);
+})
+
+app.get('/update/:updateId',(req,res)=>{
+  console.log(req.params);
+  const { updateId } = req.params;
+  res.send(`posssass= ${updateId}`);
+})
+
+app.get('/testpost/new',(req,res)=>{
+
+  res.send(`post test only`);
+
+})
+app.patch('/testpost/new',(req,res)=>{
+  console.log(req.body);
+  const {tltle}  = req.body;
+  res.send(`บันทึก ${tltle}`);
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
