4951e72ef498af912ce72e134c1631ad70a28d12
Fang <fang.live-@msn.com>
2024-12-13 14:53:53 +0700

    update port

--- models/database.js
+++ models/database.js
@@ -6,7 +6,7 @@ const connection = mysql2.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME, 
-
+  port: process.env.DB_PORT,
   });
   
   connection.connect((err)=>{
